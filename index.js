506ee33792044c6c7f71009cdeb2d2c95e3d9b49
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-08-16 11:23:13 +0100

    Implement most loyal function

--- index.js
+++ index.js
@@ -68,6 +68,64 @@ function totalSpend(email) {
   })
 }
 
-totalSpend("terry_henry@doyle.io");
+function most_loyal() {
+  // prepare requests for users and purchases
+  let users = axios.get('https://driftrock-dev-test.herokuapp.com/users').then(response => response.data.data);
+  let purchases = axios.get('https://driftrock-dev-test.herokuapp.com/purchases').then(response => response.data.data);
+  let responseData = {};
 
+  Promise.all([users, purchases]).then(values => {
+    responseData.users = values[0];
+    responseData.purchases = values[1];
+
+    return responseData;
+  })
+  .then(response => {
+    let purchases = response.purchases;
+    let allUsers = response.users;
+    let purchaseTally = {};
+    let mostPurchasesId;
+    
+    // tally the purchases by Id
+    purchases.forEach(purchase => {
+      let userId = purchase.user_id;
+      let userSpend = purchase.spend;
+
+      if (purchaseTally[userId]) {
+        purchaseTally[userId] +=  1;
+      } else {
+        purchaseTally[userId] = 1;
+      }
+    });
+
+    // find the largest spend and it's associated id
+    let spendArray = Object.values(purchaseTally);
+    let mostPurchases = Math.max(...spendArray);
+    
+    // find the id with the largest spend
+    for (let purchases in purchaseTally) {
+      if (purchaseTally[purchases] === mostPurchases) {
+        mostPurchasesId = purchases;
+      }
+    }
+
+    // find the respective id amoungst all users
+    allUsers.find(user => {
+      if (user.id === mostPurchasesId) {
+        console.log(`The largest number of purchases was made by: ${user.email}`);
+      }
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+most_loyal();
+
+// totalSpend("terry_henry@doyle.io");
 // getPurchases();
+
+// TODO:
+// implement the script commands
+  // the CLI user should be able to call the functions based arguments given in the command line
